b54627951406e2d5d46d380514b6e190f06d6dbf
Ramesh Paudel <rpaudel58@gmail.com>
2017-04-07 12:24:54 +0545

    Auth middleware to posts

--- app/Http/Controllers/UserPostController.php
+++ app/Http/Controllers/UserPostController.php
@@ -10,7 +10,7 @@ class UserPostController extends Controller
 {
 	public function __construct()
 	{
-		$this->middleware('auth')->except(['index','show']);
+		$this->middleware('auth');
 	}
 
     public function index()
