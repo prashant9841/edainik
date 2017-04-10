a1b1fb45b001df5798f5e901b62ec51e04ba51f3
Prashant Sharma <itachi9841@gmail.com>
2017-04-10 11:37:04 +0545

    dashboard design and stuff

--- resources/views/dashboard/media/index.blade.php
+++ resources/views/dashboard/media/index.blade.php
@@ -29,9 +29,9 @@
 								Image
 							</h5>
 							<div class="right">
-								<a class="btn" href="{{ url('/dashboard/medias/'. $image->id .'/edit')}}"><i class="material-icons">create</i></a>
+								<a class="btn" href="{{ url('/dashboard/medias/'. $image->id .'/edit')}}"><i class="material-icons">aspect_ratio</i></a>
+								<a class="btn" href="{{ url('dashboard/posts/'.$image->model_type::find($image->model_id)->id.'/edit') }}"><i class="material-icons">create</i></a>
 								<a class="btn red" href="{{ url('/dashboard/medias/removeImage/'.$image->id)}}"><i class="material-icons">delete</i></a>
-								<a href="{{ url('dashboard/posts/'.$image->model_type::find($image->model_id)->id.'/edit') }}">Show</a>
 							</div>
 						</div>
 					</div>
