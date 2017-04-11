34f5a90f3aa4cd94a3f612032d976c09b59e9ab7
Prashant Sharma <itachi9841@gmail.com>
2017-04-11 09:16:08 +0545

    responsive fixed

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -41,14 +41,14 @@
                 <div class="col s12 m4 logo">
                     <img src="{{asset('images/logo.png')}}" alt="">
                 </div>
-                <div class="col s12 m4">
+                <div class="col s12 m4 ads">
                     <div class="card">
                         <div class="card-content">
                             <h1>Nice and clean Ads</h1>
                         </div>
                     </div>                    
                 </div>
-                <div class="col s12 m4">
+                <div class="col s12 m4 ads">
                     <div class="card">
                         <div class="card-content">
                             <h1>Nice and clean Ads</h1>
@@ -91,7 +91,7 @@
                       breakpoint: 1024,
                       settings: {
                         slidesToShow: 3,
-                        slidesToScroll: 3,
+                        slidesToScroll: 1,
                         infinite: true,
                         dots: true
                       }
@@ -100,7 +100,7 @@
                       breakpoint: 600,
                       settings: {
                         slidesToShow: 2,
-                        slidesToScroll: 2
+                        slidesToScroll: 1
                       }
                     },
                     {
