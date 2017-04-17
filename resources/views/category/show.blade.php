11f81d05b0be4fba550b29a0b09cddf73ef95d4e
Prashant Sharma <itachi9841@gmail.com>
2017-04-17 09:20:11 +0545

    category and stylesheet fix

--- resources/views/category/show.blade.php
+++ resources/views/category/show.blade.php
@@ -24,44 +24,45 @@
         <h3>@lang('homepage.related')</h3>
         <div class="row">
             <div class="col s12 m9">
-                <ul>
-                    @foreach($related as $post)
-                        <li class="row small-post">
-                            <div class="col s4 img-div">
-                                <img src="{{ $post->getFirstImageUrl() }}" alt="">
+                <ul class="row small-post latest">
+                    @foreach($related as $news)
+                        <li class="col s12 m6">
+                            <div class="card" 
+                            {{-- style="background: url('{{$news->getFirstImageUrl()}}')  --}}
+                            ">
+                                <a href="{{ url('news',$news->slug)}}">
+                                    <div class="card-content">
+                                        <h4>{{ $news->title }}</h4>
+                                        <div class="row small">
+                                            <div class="col s6">
+                                                <p><i class="ti-time"></i> &nbsp;{{$news->created_at->diffForHumans()}}</p>
+                                            </div>
+                                        </div>
+                                        
+                                    </div>
+                                </a>
                             </div>
-                            <div class="col s8">
-                                <h4>{{ $post->title }}</h4>
-                                <p>{{ $post->description }}</p>
-                                <a href="{{ url('news',$post->slug)}}" class="right">थप पढ्नुहोस् <span></span></a>
-                                @if(Auth::check() && Auth::user()->id == $post->user_id)
-                                    <a class="btn" href="{{ url('/dashboard/posts/'.$post->slug.'/edit')}}"><i class="material-icons">create</i></a>
-                                @endif
-                            </div>
+
                         </li>
                     @endforeach
                 </ul>
             </div>
-            <div class="col s12 m3">
+            <div class="col s12 m3 side-post">
                 <div class="card">
-                    <div class="card-content">
-                        <p>Nice and Clean Ads</p>
-                    </div>
+                    {!! Ads::show('responsive') !!}
                 </div>
                 <div class="card">
                     <div class="card-content">
                         <h4>@lang('homepage.latest')</h4>
-                        <ul>
+                        <ul class="collection">
                             @foreach($related as $post)
-                                <li><a href="{{ url('news',$post->slug)}}">{{ $post->title }}</a></li>
+                               <a class="collection-item" href="{{ url('news',$post->slug)}}">{{ $post->title }}</a>
                             @endforeach
                         </ul>
                     </div>
                 </div>
                 <div class="card">
-                    <div class="card-content">
-                        <p>Nice and Clean Ads</p>
-                    </div>
+                    {!! Ads::show('responsive') !!}
                 </div>
             </div>
         </div>
