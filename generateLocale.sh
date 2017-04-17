d3e5926faaa42de46540cdce17b2ebfde889b100
Ramesh Paudel <rpaudel58@gmail.com>
2017-04-17 09:22:28 +0545

    Fixed the lang script

--- generateLocale.sh
+++ generateLocale.sh
@@ -1,6 +1,6 @@
 #!bin\bash
 echo "**********************************"
 echo "Copying the nepali language files "
-cp ./resources/lang/ne/dates.php ./vendor/nesbot/src/Carbon/Lang/ne.php
+cp ./resources/lang/ne/dates.php ./vendor/nesbot/carbon/src/Carbon/Lang/ne.php
 echo
 echo
