88517bfe4d43d4ca9bceb823487544c0cb97876e
Ramesh Paudel <rpaudel58@gmail.com>
2017-04-19 09:19:28 +0545

    Fixed Url

--- resources/views/layouts/_frontendMenu.blade.php
+++ resources/views/layouts/_frontendMenu.blade.php
@@ -1,7 +1,7 @@
 
 @foreach($menus as $menu)
     <li><a class="
-    @if(URL::current() == url('/categories/',$menu->category->slug))
+    @if(url()->current() == url('/categories/',$menu->category->slug))
         active
     @endif
     " href="{{ url('/categories/'.$menu->category->slug ) }}">{{$menu->category->title}}</a></li>
