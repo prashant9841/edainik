33df612b445044a6912ec5d8f8b7502633d66780
Ramesh Paudel <rpaudel58@gmail.com>
2017-04-19 18:41:07 +0545

    Added consumer ID

--- config/ads.php
+++ config/ads.php
@@ -1,10 +1,11 @@
 <?php
 return [
-    'client_id' => 'YOUR_CLIENT_ID', //Your Adsense client ID e.g. ca-pub-9508939161510421
+    'client_id' => 'pub-4280932567494358', //Your Adsense client ID e.g. ca-pub-9508939161510421
     'ads' => [
         'responsive' => [
             'ad_unit_id' => 12345678901,
-            'ad_format' => 'auto'
+            'ad_format' => 'auto',
+            'ad_style' => 'display:inline-block',
         ],
         'rectangle' => [
             'ad_unit_id' => 1234567890,
