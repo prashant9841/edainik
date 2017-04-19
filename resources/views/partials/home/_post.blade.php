1088ce20f1a2c26e2cc766a460806bb8c55bc34a
Prashant Sharma <itachi9841@gmail.com>
2017-04-19 11:30:53 +0545

    sharing thing

--- resources/views/partials/home/_post.blade.php
+++ resources/views/partials/home/_post.blade.php
@@ -9,9 +9,11 @@
         <div class="section">
             @if($post->category)
                 <p>{{ $post->category->title }} </p>
-                <div class="row share center"> </div>
-                <div class="addthis_inline_share_toolbox centre"></div>
+                {{-- <div class="row share center"> </div> --}}
             @endif
+            <div class="icon-wrap">
+                <div class="addthis_inline_share_toolbox"></div>
+            </div>
         </div>
         <div class="parallax">
             @if($post->getMedia('images')->count() > 0)
