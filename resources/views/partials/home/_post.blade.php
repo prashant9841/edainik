3e7317ed3a99f9c1a9a79c65960317eba17f87da
Prashant Sharma <itachi9841@gmail.com>
2017-04-21 09:48:03 +0545

    some style changes

--- resources/views/partials/home/_post.blade.php
+++ resources/views/partials/home/_post.blade.php
@@ -1,7 +1,19 @@
 <div class="large-post">        
     <div class="section">
         <div class="container">
-            <h1 class="header center">{{$post->title}}</h1>
+            <h1 class="header center">{{$post->title}}</h1>            
+            <div class="row center">
+                <div class="btn {{$post->category->header_color}} ">
+                    {{$post->category->title}}
+                    <img src="
+                    @if(strlen($post->category->icon) >4)
+                        {{asset('/images/icons/'.$post->category->icon)}}
+                    @else
+                        {{asset('/images/icons/news.png')}}
+                    @endif
+                    " style="width: 20px; margin: 0 0 -3px 15px; height: 20px;" alt="{{$post->category->title}}">                    
+                </div>
+            </div>
             
         </div>
     </div>
