60000e828fc8b49331b57ff3c8d02df9582df817
Ramesh Paudel <rpaudel58@gmail.com>
2017-04-21 14:49:06 +0545

    Not found fix

--- resources/views/partials/home/_notFound.blade.php
+++ resources/views/partials/home/_notFound.blade.php
@@ -1,5 +1,8 @@
-<li class="col s12 m6">
-	<h5 class="card">
-		<i class="material-icons">info_outline</i>&nbsp; @lang('homepage.no-item-found')
-	</h5>
+<li class="container col s12 m6" style="margin-left: 43px">
+	<div class="card">
+		<div class="card-content">
+			<h5><i class="material-icons">info_outline</i>&nbsp; @lang('homepage.no-item-found')</h5>
+		</div>
+		
+	</div>
 </li>
