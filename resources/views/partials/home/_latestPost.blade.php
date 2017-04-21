2dcf6d55ff3da2363604e5ec6a4d12071ccc3f46
Ramesh Paudel <rpaudel58@gmail.com>
2017-04-21 08:40:36 +0545

    Added not found

--- resources/views/partials/home/_latestPost.blade.php
+++ resources/views/partials/home/_latestPost.blade.php
@@ -1,12 +1,17 @@
-<ul class="row tiny-post latest">
-    @foreach($latestNews as $news)
-        <li>
-            <a href="{{ url('news',$news->slug)}}">
-                
-                        <h4>{{ $news->title }}</h4>
-                    
-            </a>
+@if($latestNews->count())
+	<ul class="row tiny-post latest">
+	    @foreach($latestNews as $news)
+	        <li>
+	            <a href="{{ url('news',$news->slug)}}">
+	                
+	                        <h4>{{ $news->title }}</h4>
+	                    
+	            </a>
 
-        </li>
-    @endforeach
-</ul>
+	        </li>
+	    @endforeach
+	</ul>
+@else
+	<div class="card">
+		@lang('homepage.no-item-found')
+	</div>
