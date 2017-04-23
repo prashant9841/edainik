cb06768c2b28e6a272e90cb3c3e96195de63099d
zoro <itachi9841@gmail.com>
2017-04-23 13:12:35 +0545

    responsive fix

--- resources/views/partials/home/_notFound.blade.php
+++ resources/views/partials/home/_notFound.blade.php
@@ -1,4 +1,4 @@
-<li class="container col s12 m6" style="margin-left: 43px">
+<li class="container col s12 m6 notfound" >
 	<div class="card">
 		<div class="card-content">
 			<h5><i class="material-icons">info_outline</i>&nbsp; @lang('homepage.no-item-found')</h5>
