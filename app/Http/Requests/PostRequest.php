e5c786d0d24e9d74d9bc4b8777b4aca01bb9fcab
ramesh poudel <rpaudel58@gmail.com>
2017-04-23 16:10:06 +0545

    fixed erroe

--- app/Http/Requests/PostRequest.php
+++ app/Http/Requests/PostRequest.php
@@ -28,7 +28,7 @@ class PostRequest extends FormRequest
             'title' => 'required|min:5|max:250',
             'content' => 'required',
             'status' => 'required',
-            'image' => 'mimes:jpeg,jpg,png',
+
 
         ];
     }
