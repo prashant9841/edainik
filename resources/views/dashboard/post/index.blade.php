a26d79921589221e2c9dfed028b1af98de777e6b
Ramesh Paudel <rpaudel58@gmail.com>
2017-04-23 13:51:15 +0545

    Added featured to posts

--- resources/views/dashboard/post/index.blade.php
+++ resources/views/dashboard/post/index.blade.php
@@ -48,6 +48,15 @@
 									href="{{ url('/dashboard/posts/'.$post->id.'/delete') }}"><i class="material-icons">delete_forever</i></button>
 								</form>
 								@if(Auth::user()->isSuperAdmin())
+										@if(isset($post->featured) && $post->featured->count())
+											<a class="btn red tooltipped" 
+											data-position="bottom" data-delay="50" data-tooltip="Remove from featured"
+											href="{{ url('/dashboard/posts/'.$post->id.'/featured') }}"><i class="material-icons">star</i></a>
+										@else
+											<a class="btn green tooltipped" 
+											data-position="bottom" data-delay="50" data-tooltip="Add as featured"
+											href="{{ url('/dashboard/posts/'.$post->id.'/featured') }}"><i class="material-icons">star</i></a>
+										@endif
 									@if($post->verified)
 										<a class="btn red tooltipped" 
 										data-position="bottom" data-delay="50" data-tooltip="Unverify"
