d9ab7ab15cef06148887e5f68edfc7eb20bd60b4
Prashant Sharma <itachi9841@gmail.com>
2017-04-23 14:16:06 +0545

    removed the jS bug

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -121,49 +121,6 @@
                 url: "http://www.edainikpost.com:2020/"
             });
             */
-             $('#flash-news').slick({
-                dots: true,
-                infinite: true,
-                speed: 600,
-                autoplay: true,
-                slidesToShow: 4,
-                slidesToScroll: 1,
-                responsive: [
-                    {
-                      breakpoint: 1024,
-                      settings: {
-                        slidesToShow: 3,
-                        slidesToScroll: 1,
-                        infinite: true,
-                        dots: true
-                      }
-                    },
-                    {
-                      breakpoint: 600,
-                      settings: {
-                        slidesToShow: 2,
-                        slidesToScroll: 1
-                      }
-                    },
-                    {
-                      breakpoint: 480,
-                      settings: {
-                        slidesToShow: 1,
-                        slidesToScroll: 1
-                      }
-                    }
-                    // You can unslick at a given breakpoint now by adding:
-                    // settings: "unslick"
-                    // instead of a settings object
-                ]
-            });
-             $('.slide-news').slick({
-                dots: false,
-                autoplay: true,
-                arrow: false
-                // adaptiveHeight: true
-
-             })
         </script>
 
     @stack('scripts')
