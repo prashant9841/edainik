20ae269821b5ffa4e7f769d71725d4664d9b22d3
Prashant Sharma <itachi9841@gmail.com>
2017-04-24 14:05:52 +0545

    removed the btns and anchors addeded in headings

--- resources/views/partials/category/_postCard.blade.php
+++ resources/views/partials/category/_postCard.blade.php
@@ -6,21 +6,16 @@
                         <img src="{{ $post->getFirstImageUrl('small') }}" alt="{{ $post->title }}">
                     </div>                            
                     <div class="card-content col s12 l6">
-                        <h1 class="header center">{{$post->title}}</h1>
-                        <div class="row small">
+                        <a href="{{ route('singleNews',$post->slug) }}" ><h1 class="header center">{{$post->title}}</h1></a>
+                        {{-- <div class="row small">
                              <div class="col s6">
                                 <p><i class="fa fa-user"></i> &nbsp;{{$post->author}}</p>
                             </div>
                             <div class="col s6">
                                 <p><i class="fa fa-map"></i>&nbsp; {{ $post->address}}</p>
                             </div>
-                        </div>
+                        </div> --}}
                         <p class="wrap">{{ $post->description }}</p>
-                        {{-- <div class="row share center"> </div> --}}
-                        <div class="row center btn-row">
-
-                            <a href="{{ route('singleNews',$post->slug) }}" class="btn waves-effect waves-light">@lang('homepage.read-more')</a>
-                        </div>
                 </div>
                 </div>
             </div>
