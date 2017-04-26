013a0f8a1c67780db24475b9eeb9e4f04d2f1786
Prashant Sharma <itachi9841@gmail.com>
2017-04-26 08:18:09 +0545

    font sizes fixes

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -69,7 +69,7 @@
                 </div>
                 <div class="row">
                     <div class="col s12 m9">
-                        <div style="text-align: justify;">{!!$post->content!!}</div>
+                        <div class="post-content">{!!$post->content!!}</div>
                         <br>
                         <div> <i class="fa fa-clock-o"></i> &nbsp; {{$post->getNepaliDate($post->created_at)}} </div>
                         
