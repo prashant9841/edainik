dc4a01a81837c2b5bce4a15a841a776a4542c55c
Ramesh Paudel <rpaudel58@gmail.com>
2017-04-26 12:46:11 +0545

    Maybe

--- app/Http/Controllers/Site/PagesController.php
+++ app/Http/Controllers/Site/PagesController.php
@@ -12,11 +12,11 @@ class PagesController extends Controller
     {
         $approvedCategory = $category->where('status',1);
     	return view('welcome')
-    	->with('posts', $this->getFeatured($post)) //This is featured Post
-    	->with('latestNews', $post->approved()->latest()->take(20)->get())
-    	->with('trendingNews', $this->getTrending())
-        ->with('categoriesList', $approvedCategory->where('on_homepage',true)->get())
-        ->with('sidebarCategory', $approvedCategory->where('on_sidebar',true)->get());
+        ->with('sidebarCategory', $approvedCategory->where('on_sidebar',true)->get())
+        ->with('posts', $this->getFeatured($post)) //This is featured Post
+        ->with('latestNews', $post->approved()->latest()->take(20)->get())
+        ->with('trendingNews', $this->getTrending())
+        ->with('categoriesList', $approvedCategory->where('on_homepage',true)->get());
     }
 
     protected function getTrending()
